c1c15dfcc54d1bbcf4a58644f2937ddfbaf6715e
Thibault Ballof <moi@thibault-ballof.fr>
2021-10-23 16:20:59 +0200

    Add test division

--- CountOnMeTests/CountOnMeTests.swift
+++ CountOnMeTests/CountOnMeTests.swift
@@ -18,10 +18,20 @@ class CountOnMeTests: XCTestCase {
         XCTAssertEqual(testCalcul.calcul(enterNumber: ["18", "+", "2"]), 20)
         XCTAssertEqual(testCalcul.calcul(enterNumber: ["17", "+", "13"]), 30)
     }
-    func testSoustraction() {
+    func testSubstraction() {
         XCTAssertEqual(testCalcul.calcul(enterNumber: ["55", "-", "5"]), 50)
         XCTAssertEqual(testCalcul.calcul(enterNumber: ["18", "-", "2"]), 16)
         XCTAssertEqual(testCalcul.calcul(enterNumber: ["13", "-", "17"]), -4)
+    }
+    func testMultiplication() {
+        XCTAssertEqual(testCalcul.calcul(enterNumber: ["55", "*", "5"]), 275)
+        XCTAssertEqual(testCalcul.calcul(enterNumber: ["18", "*", "2"]), 36)
+        XCTAssertEqual(testCalcul.calcul(enterNumber: ["13", "*", "17"]), 221)
+    }
+    func testDivision() {
+        XCTAssertEqual(testCalcul.calcul(enterNumber: ["55", "/", "5"]), 11)
+        XCTAssertEqual(testCalcul.calcul(enterNumber: ["18", "/", "2"]), 9)
+        XCTAssertEqual(testCalcul.calcul(enterNumber: ["13", "/", "17"]), 0)
     }
 
 }
